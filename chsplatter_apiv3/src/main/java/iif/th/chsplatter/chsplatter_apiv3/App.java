08995a076ae423960f515e421ca126941ed69d3b
notmattlythgoe <notmattlythgoe@gmail.com>
2019-07-15 08:10:14 -0400

    Scale wins

--- chsplatter_apiv3/src/main/java/iif/th/chsplatter/chsplatter_apiv3/App.java
+++ chsplatter_apiv3/src/main/java/iif/th/chsplatter/chsplatter_apiv3/App.java
@@ -19,20 +19,67 @@ import net.thefletcher.tbaapi.v3client.api.DistrictApi;
 import net.thefletcher.tbaapi.v3client.api.EventApi;
 import net.thefletcher.tbaapi.v3client.api.TeamApi;
 import net.thefletcher.tbaapi.v3client.auth.ApiKeyAuth;
+import net.thefletcher.tbaapi.v3client.model.Award;
+import net.thefletcher.tbaapi.v3client.model.AwardRecipient;
 import net.thefletcher.tbaapi.v3client.model.EliminationAlliance;
 import net.thefletcher.tbaapi.v3client.model.Event;
+import net.thefletcher.tbaapi.v3client.model.EventRanking;
+import net.thefletcher.tbaapi.v3client.model.EventRankingRankings;
+import net.thefletcher.tbaapi.v3client.model.EventSimple;
 import net.thefletcher.tbaapi.v3client.model.Team;
 
 public class App {
 	
-	public static void main(String... args) {
+	public static void main(String... args) throws Exception {
 		ApiClient defaultClient = Configuration.getDefaultApiClient();
+		
 
 		// Configure API key authorization: apiKey
 		ApiKeyAuth apiKey = (ApiKeyAuth) defaultClient.getAuthentication("apiKey");
 		apiKey.setApiKey("epPoAKTstHeNGcwOxIQX8AuN4wfzkodaHIqmChzbqgKPG0tY1xzTOSTMH8FOpfoc");
 		// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
 		//apiKey.setApiKeyPrefix("Token");
+
+		runChsPlatter();
+		// calculateAvergeAwardRank();
+	}
+
+	private static void calculateAvergeAwardRank() throws Exception{
+		EventApi eventApi = new EventApi();
+		TeamApi teamApi = new TeamApi();
+
+		List<EventSimple> events = eventApi.getEventsByYearSimple(new BigDecimal(2019), null);
+		double sum = 0;
+		int total = 0;
+		for(EventSimple event : events) {
+			if (event.getEventType() != 0) {
+				continue;
+			}
+			for (Award award : eventApi.getEventAwards(event.getKey(), null)) {
+				if (
+					award.getAwardType() == 0 
+					// || award.getAwardType() == 1 
+					// || award.getAwardType() == 9 
+					// || award.getAwardType() == 10
+					) {
+						for (AwardRecipient recipient : award.getRecipientList()) {
+							EventRanking ranking = eventApi.getEventRankings(event.getKey(), null);
+							for (EventRankingRankings rank : ranking.getRankings()) {
+								if (rank.getTeamKey().equals(recipient.getTeamKey())) {
+									double teamRank = rank.getRank();
+									sum += (teamRank / ranking.getRankings().size());
+									total++;
+									break;
+								}
+							}
+						}
+					}
+			}
+		}
+		System.out.println(sum / total);
+	}
+
+	private static void runChsPlatter() {
 
 		EventApi eventApi = new EventApi();
 		DistrictApi districtApi = new DistrictApi();
@@ -84,18 +131,20 @@ public class App {
 									for (int p = 0; p < 3; p++) {
 										String pick = alliance.getPicks().get(p);
 										if (pick.equals(team.getKey())) {
+											double eventPoints = 0;
 											if (p < 2) {
-												numPoints += (16 - a) * (yearsToAverage - i);
+												eventPoints = 16 - a;
 											} else {
-												numPoints += (a + 1) * (yearsToAverage - i);
+												eventPoints = a + 1;
 											}
 											if ("won".equals(alliance.getStatus().getStatus())) {
-												numPoints += 9  * (yearsToAverage - i);
+												eventPoints *= 2;
 											} else if ("f".equals(alliance.getStatus().getLevel())) {
-												numPoints += 6;
+												eventPoints *= 1.66;
 											} else if ("sf".equals(alliance.getStatus().getLevel())) {
-												numPoints += 3  * (yearsToAverage - i);
+												eventPoints *= 1.33;
 											} 
+											numPoints += eventPoints  * (yearsToAverage - i);
 										}
 									}
 								}
